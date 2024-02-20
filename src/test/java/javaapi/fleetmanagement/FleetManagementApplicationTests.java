884ff954a736149a512eea3a1383c5526cc1408b
KarlaMacedo <karlapaolamacedo@gmail.com>
2024-02-20 10:03:54 -0600

    Upload trajectories´s integration test

--- src/test/java/javaapi/fleetmanagement/FleetManagementApplicationTests.java
+++ src/test/java/javaapi/fleetmanagement/FleetManagementApplicationTests.java
@@ -1,6 +1,7 @@
 package javaapi.fleetmanagement;
 
 import javaapi.fleetmanagement.repositories.TaxiRepository;
+import javaapi.fleetmanagement.repositories.TrajectoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +24,31 @@ class FleetManagementApplicationTests {
 
 	@Test
 	@DisplayName("TestFirstEndPoint")
-	void contextLoads() throws Exception {
+	void testFirstEndPointContextLoads() throws Exception {
 		mockMvc.perform(get("/taxi?page=0&size=10")).andExpect(status().isOk())
 				.andExpect(jsonPath("$.content").isArray())
 				.andExpect(jsonPath("$.content[0].id").exists())
 				.andExpect(jsonPath("$.content[0].plate").exists())
 				.andExpect(jsonPath("$.content.length()").value(10));
+	}
+
+	@Autowired
+	private TrajectoryRepository trajectoryRepository;
+
+	@Test
+	@DisplayName("TestSecondEndPoint")
+	void testSecondEndPointContextLoads() throws Exception {
+		mockMvc.perform(get("/trajectory/7957?date=2008-02-02&page=0&size=20")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").isArray())
+				.andExpect(jsonPath("$.content[0].id").exists())
+				.andExpect(jsonPath("$.content[0].taxi.id").exists())
+				.andExpect(jsonPath("$.content[0].taxi.plate").exists())
+				.andExpect(jsonPath("$.content[0].date").exists())
+				.andExpect(jsonPath("$.content[0].latitude").exists())
+				.andExpect(jsonPath("$.content[0].longitude").exists())
+				.andExpect(jsonPath("$.content[0].taxi.id").value(7957))
+				.andExpect(jsonPath("$.content[0].date").value("2008-02-02"))
+				.andExpect(jsonPath("$.content.length()").value(20));
 	}
 
 }
